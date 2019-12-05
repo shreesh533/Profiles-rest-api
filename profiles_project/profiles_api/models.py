8569e4770adf02b60e7d5e123d6b0c04def9ceb7
Shreesh Pralhad Katti <shreesh.katti@tallysolutions.com>
2019-12-05 17:30:33 +0530

    Python userprofile models

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -1,3 +1,68 @@
+# Djago comes with default database user model.
+# We are going to override the default model and write our own model which takes email instead of username for authentication.
+# We create UserProfile models.
+# AbstractBaseUser and PermissionMixin are standard base classes we need to use when we customize the default django user model.
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile."""
+        if not email:
+             raise ValueError("User must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save new superuser with given details """
+        user = self.craete_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return users
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for users in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrive full name of a user"""
+        return self.name
+
+    def get_short_names(self):
+        """Retrive short name of a user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
+
+
+
+# class Musician(models.Model):
+#     first_name = models.CharField(max_length=200)
+#     last_name = models.CharField(max_length=200)
+#     instrument = models.CharField(max_length=200)
+#
+#
+# class Album(models.Model):
+#     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#     release_date = models.DateField()
+#     num_stars = models.IntegerField()
